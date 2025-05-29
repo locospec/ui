627ee8166c64d4b515a76429d7640ead20458860
Rajiv Seelam <rajiv@betalectic.com>
2025-05-29 16:06:11 +0530

    feat: contribute changes from local to source

---
+++ components/locospecui/base/index.ts
@@ -0,0 +1,24 @@
+export {
+  Dialog,
+  DialogPortal,
+  DialogOverlay,
+  DialogTrigger,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle,
+  DialogDescription,
+} from "./dialog";
+
+export {
+  Command,
+  CommandDialog,
+  CommandInput,
+  CommandList,
+  CommandEmpty,
+  CommandGroup,
+  CommandItem,
+  CommandShortcut,
+  CommandSeparator,
+} from "./command";
